f5c1648fedd075f289d0065cb9ba910c16699e3f
Nikolay <Onedaydejavu124@mail.ru>
2022-09-23 23:36:16 +0700

    В матрице чисел найти сумму главной диагонали

---
+++ EX004/Program.cs
@@ -0,0 +1,32 @@
+﻿// В матрице чисел найти сумму главной диагонали
+
+void FillMatrix(int[,] matrix, int min, int max)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i,j] = new Random().Next(min, max);
+
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i,j]} ");
+        Console.WriteLine();
+    }
+}
+void SumMatrix(int[,] matrix)
+{
+    int sum = 0;
+    for(int i = 0; i< matrix.GetLength(0); i++)
+    {
+        sum = sum + matrix[i,i];
+    }
+    Console.Write($"Сумма элементов массива по диагонали = {sum}");
+}
+
+int [,] matrix = new int[5,5];
+FillMatrix(matrix, 1, 10);
+PrintMatrix(matrix);
+SumMatrix(matrix);
