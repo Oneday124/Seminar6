c947bdfa3e43469bab3d83848e6ef467b222de77
Nikolay <Onedaydejavu124@mail.ru>
2022-09-23 22:55:49 +0700

    В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет

---
+++ EX003/Program.cs
@@ -0,0 +1,45 @@
+﻿// В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет
+
+void FillMatrix(int[,] matrix, int min, int max)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i,j] = new Random().Next(min, max);
+
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i,j]} ");
+        Console.WriteLine();
+    }
+}
+void Search(int[,] matrix, int num)
+{
+    int search = 0;
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(matrix[i,j] == num) 
+            {
+                Console.WriteLine($"Позиция {num}: [{i},{j}]");
+                search = matrix[i,j];
+            }
+        }
+    }
+    if(search == 0)
+    {
+        Console.WriteLine("Такого элемента нет");
+    }
+}
+
+int [,] matrix = new int[5,5];
+FillMatrix(matrix, 1, 10);
+PrintMatrix(matrix);
+Console.WriteLine("Введите значение элемента массива: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+Search(matrix, number);
+
